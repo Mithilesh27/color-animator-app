170b0effc7f216a0eeeac7e2d54ffa558ff92fa2
Mithilesh Kumar Patel <31652458+Mithilesh27@users.noreply.github.com>
2022-03-26 17:37:01 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,88 @@
+
+
+
+function funcPrimary(){
+
+    el=document.getElementById('primary');
+    el.setAttribute("class" , "col-3 bg-primary");
+}
+function funcSecondary(){
+
+    el=document.getElementById('secondary');
+    el.setAttribute("class" , "col-3 bg-secondary");
+}
+function funcSuccess(){
+
+    el=document.getElementById('success');
+    el.setAttribute("class" , "col-3 bg-success");
+}
+function funcDanger(){
+
+    el=document.getElementById('danger');
+    el.setAttribute("class" , "col-3 bg-danger");
+}
+function funcWarning(){
+
+    
+    el=document.getElementById('warning');
+    el.setAttribute("class" , "col-3 bg-warning");
+}
+function funcInfo(){
+
+    el=document.getElementById('info');
+    el.setAttribute("class" , "col-3 bg-info");
+}
+function funcLight(){
+
+    el=document.getElementById('primary');
+    el.setAttribute("class" , "col-3 bg-light");
+}
+function funcDark(){
+
+    el=document.getElementById('dark');
+    el.setAttribute("class" , "col-3 bg-dark");
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function stopPrimary() {
+    el=document.getElementById('primary');
+    el.setAttribute("class" , "col-3");
+
+    
+}
+function stopSecondary(){
+
+    el=document.getElementById('secondary');
+    el.setAttribute("class" , "col-3");
+}
+function stopSuccess(){
+
+    el=document.getElementById('success');
+    el.setAttribute("class" , "col-3");
+}
+function stopDanger(){
+
+    el=document.getElementById('danger');
+    el.setAttribute("class" , "col-3");
+}
+function stopWarning(){
+
+    
+    el=document.getElementById('warning');
+    el.setAttribute("class" , "col-3");
+}
+function stopInfo(){
+
+    el=document.getElementById('info');
+    el.setAttribute("class" , "col-3");
+}
+function stopLight(){
+
+    el=document.getElementById('primary');
+    el.setAttribute("class" , "col-3");
+}
+function stopDark(){
+
+    el=document.getElementById('dark');
+    el.setAttribute("class" , "col-3");
+}
